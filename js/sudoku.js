901d364955b11e9736f883cf6dca64f8811f2a85
Pavel <pavelblr02@mail.ru>
2017-05-30 01:01:22 +0400

    add shuffleCounter

--- js/sudoku.js
+++ js/sudoku.js
@@ -12,11 +12,25 @@ let	sudokuField=[
 	[9,1,2,3,4,5,6,7,8]
 ]
 
-function RandomDiap(N,M) {
+function RandomDiap_for_i(N,M) {
     return Math.floor(
         Math.random()*(M-N+1)
         )+N;
 }
+
+function RandomDiap_for_blocks(N,M) {
+    var x = Math.floor(
+        Math.random()*(M-N+1)
+        )+N;
+    if (x>=0 && x<=3) {
+    	return 0;
+    } else 
+    if (x>=3 && x<=6) {
+    	return 3;
+    } else return 6;
+}
+
+console.log(RandomDiap_for_blocks(1,9))
 //////// заменяем столбцы на строки
 var transposingArr = sudokuField.map(function(col, i) { 
   return sudokuField.map(function(row) { 
@@ -26,48 +40,67 @@ var transposingArr = sudokuField.map(function(col, i) {
 
 
 function shuffle_i(x,y) {
-	var b = sudokuField.splice(x,1);
-	sudokuField.splice(y,0,b[0])
+	var b = transposingArr.splice(x,1);
+	transposingArr.splice(y,0,b[0]);
 }
+
 function shuffle_block(x,y) {
-	var b = sudokuField.splice(x,3);
-	sudokuField.splice(y,0,b[0],b[1],b[2])
+	var b = transposingArr.splice(x,3);
+	transposingArr.splice(y,0,b[0],b[1],b[2]);
 }
 
 function shuffle_Vertical(x,y) {
-	for (var i = 0; i < sudokuField.length; i++) {
-		var b = sudokuField[i].splice(x,1);
-		sudokuField[i].splice(y,0,b);
+	for (var i = 0; i < transposingArr.length; i++) {
+		var b = transposingArr[i].splice(x,1);
+		transposingArr[i].splice(y,0,b);
 	}
 }
+
 function shuffle_Vertical_block(x,y) {
-	for (var i = 0; i < sudokuField.length; i++) {
-		var b = sudokuField[i].splice(x,3);
-		sudokuField[i].splice(y,0,b[0],b[1],b[2]);
+	for (var i = 0; i < transposingArr.length; i++) {
+		var b = transposingArr[i].splice(x,3);
+		transposingArr[i].splice(y,0,b[0],b[1],b[2]);
 	}
 }
 
 
-/////// тасование блоков по вертикали;
-// shuffle_Vertical_block(0,3)
+// ///// тасование блоков по вертикали;
+// shuffle_Vertical_block(RandomDiap_for_blocks(1,9),RandomDiap_for_blocks(1,9))
 
-/////// тасование  по вертикали в блоках
-// shuffle_Vertical(0,0)
-
-////// тасование блоков по горизонтали
-// shuffle_block(6,3)
-
-
-////// Тасование по горизонтали в блоках
-// var xRandom_1 = RandomDiap(0,2);
-// var yRandom_1 = RandomDiap(0,2);
-// shuffle_i(xRandom_1,yRandom_1);
-// shuffle_i(RandomDiap(3,5),RandomDiap(3,5))
-// shuffle_i(RandomDiap(6,8),RandomDiap(6,8))
-
-console.log(sudokuField);
+// ///// тасование  по вертикали в блоках
+// shuffle_Vertical(RandomDiap_for_i(0,2),RandomDiap_for_i(0,2))
+// shuffle_Vertical(RandomDiap_for_i(3,5),RandomDiap_for_i(3,5))
+// shuffle_Vertical(RandomDiap_for_i(6,8),RandomDiap_for_i(6,8))
+// //// тасование блоков по горизонтали
+// shuffle_block(RandomDiap_for_blocks(1,9),RandomDiap_for_blocks(1,9))
 
 
+// //// Тасование по горизонтали в блоках
+// shuffle_i(RandomDiap_for_i(0,2),RandomDiap_for_i(0,2));
+// shuffle_i(RandomDiap_for_i(3,5),RandomDiap_for_i(3,5))
+// shuffle_i(RandomDiap_for_i(6,8),RandomDiap_for_i(6,8))
+
+function counShuffle(count) {
+	
+	while (count>0) {
+		shuffle_Vertical_block(RandomDiap_for_blocks(1,9),RandomDiap_for_blocks(1,9))
+		
+		///// тасование  по вертикали в блоках
+		shuffle_Vertical(RandomDiap_for_i(0,2),RandomDiap_for_i(0,2))
+		shuffle_Vertical(RandomDiap_for_i(3,5),RandomDiap_for_i(3,5))
+		shuffle_Vertical(RandomDiap_for_i(6,8),RandomDiap_for_i(6,8))
+		//// тасование блоков по горизонтали
+		shuffle_block(RandomDiap_for_blocks(1,9),RandomDiap_for_blocks(1,9))
+		
+		
+		//// Тасование по горизонтали в блоках
+		shuffle_i(RandomDiap_for_i(0,2),RandomDiap_for_i(0,2));
+		shuffle_i(RandomDiap_for_i(3,5),RandomDiap_for_i(3,5))
+		shuffle_i(RandomDiap_for_i(6,8),RandomDiap_for_i(6,8))
+		count--
+	}
+}
+counShuffle(1000)
 var field = document.getElementById('field')
 
 function fieldFPS () {
@@ -85,6 +118,3 @@ function fieldFPS () {
 }
 fieldFPS ()
 
-// leftblock.sort(compareRandom)
-
-// console.log(leftblock)
